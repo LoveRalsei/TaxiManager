820da2708dc0f0b1be398f4464387c80badd22a2
SaltfishSheep <saltfishsheep@hotmail.com>
2026-04-15 22:20:18 +0800

    加载速度优化

--- WinFormsApp1/DataLoader.cs
+++ WinFormsApp1/DataLoader.cs
@@ -1,7 +1,9 @@
 using Microsoft.VisualBasic;
 using System;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Globalization;
+using System.IO;
 using System.IO.Compression;
 using System.Linq;
 using System.Numerics;
@@ -37,10 +39,10 @@ namespace TaxiManager
             Nodes = nodes;
         }
         public Driver(PathSupplier pathSupplier) : this(pathSupplier()) { }
-        public Driver(ZipArchiveEntry entry) : this(new PathSupplier(() =>
+        public Driver(Stream stream) : this(new PathSupplier(() =>
         {
-            LinkedList<PathNode> nodes = [];
-            using (var stream = entry.Open())
+            // 较宽松地预估长度
+            List<PathNode> nodes = [];
             using (StreamReader reader = new(stream))
             {
                 const double dTolerance = 0.00005;// 约 5 米的容忍度，用于修正GPS误差和压缩不动的节点。
@@ -48,10 +50,38 @@ namespace TaxiManager
                 DateTime? ignoredTime = null;
                 for (var line = reader.ReadLine(); line != null && line.Length > 0; line = reader.ReadLine())
                 {
-                    var splited = line.Split(',');
-                    var date = DateTime.ParseExact(splited[1], PathNode.DateFormat, CultureInfo.InvariantCulture);
-                    var x = double.Parse(splited[2]);
-                    var y = double.Parse(splited[3]);
+                    // 使用 Span 避免字符串分配
+                    ReadOnlySpan<char> lineSpan = line.AsSpan();
+
+                    // 手动解析 CSV，避免 Split 的数组分配
+                    int firstComma = lineSpan.IndexOf(',');
+                    if (firstComma < 0) continue;
+
+                    int secondComma = lineSpan.Slice(firstComma + 1).IndexOf(',');
+                    if (secondComma < 0) continue;
+                    secondComma += firstComma + 1;
+
+                    int thirdComma = lineSpan.Slice(secondComma + 1).IndexOf(',');
+                    if (thirdComma < 0) continue;
+                    thirdComma += secondComma + 1;
+
+                    // 解析日期
+                    ReadOnlySpan<char> dateSpan = lineSpan.Slice(firstComma + 1, secondComma - firstComma - 1);
+                    if (!DateTime.TryParseExact(dateSpan, PathNode.DateFormat,
+                        CultureInfo.InvariantCulture, DateTimeStyles.None, out DateTime date))
+                    {
+                        continue;
+                    }
+
+                    // 解析坐标
+                    ReadOnlySpan<char> xSpan = lineSpan.Slice(secondComma + 1, thirdComma - secondComma - 1);
+                    ReadOnlySpan<char> ySpan = lineSpan.Slice(thirdComma + 1);
+
+                    if (!double.TryParse(xSpan, NumberStyles.Float, CultureInfo.InvariantCulture, out double x) ||
+                        !double.TryParse(ySpan, NumberStyles.Float, CultureInfo.InvariantCulture, out double y))
+                    {
+                        continue;
+                    }
                     if (!Position.IsValid(x, y))
                         continue;
                     // 如果当前节点和上一个节点的位置相差大于容忍度，则认为是新的有效节点。
@@ -61,19 +91,21 @@ namespace TaxiManager
                         if (ignoredTime != null)
                         {
                             var tailNode = new PathNode(ignoredTime.Value, lastX, lastY);
-                            nodes.AddLast(tailNode);
+                            nodes.Add(tailNode);
                         }
                         lastX = x;
                         lastY = y;
                         var node = new PathNode(date, x, y);
-                        nodes.AddLast(node);
+                        nodes.Add(node);
                     } else
                     {
                         ignoredTime = date;
                     }
                 }
             }
-            return [.. nodes];
+            // 除去多余项
+            nodes.TrimExcess();
+            return nodes;
         })) { }
 
         /// <summary>
@@ -244,6 +276,38 @@ namespace TaxiManager
 
         private static Task? _loadTask;
 
+        private static long _loadDiskMs = 0;
+        public static long LoadDiskMs => _loadDiskMs;
+        private static long _loadTotalMs = 0;
+        public static long LoadTotalMs => _loadTotalMs; 
+
+        private static Func<Driver?> _getEntryStreamHandler(Stream stream)
+        {
+            return () =>
+            {
+                try
+                {
+                    var driver = new Driver(stream);
+                    if (!driver.IsEmpty)
+                        Interlocked.Increment(ref _driversCount);
+                    else
+                        driver = null;
+                    // 原子操作计数
+                    Interlocked.Increment(ref _loadedCount);
+                    return driver;
+                }
+                catch (Exception ex)
+                {
+                    _error = ex;
+                }
+                finally
+                {
+                    stream.Dispose();
+                }
+                return null;
+            };
+        }
+
         public static void Load()
         {
             if (Loaded)
@@ -268,24 +332,36 @@ namespace TaxiManager
             {
                 try
                 {
-                    LinkedList<Driver> drivers = new();
+                    var firstTime = DateTime.Now;
+                    List<Driver> drivers = [];
+                    LinkedList<Task<Driver?>> entryLoaders = [];
                     var zipFiles = Directory.GetFiles(_dataPath, "*.zip");
                     foreach (var zipFile in zipFiles)
                     {
                         using var zip = ZipFile.OpenRead(zipFile);
                         foreach (var entry in zip.Entries)
                         {
-                            var driver = new Driver(entry);
-                            if (!driver.IsEmpty)
-                            {
-                                drivers.AddLast(driver);
-                                driver.Id = drivers.Count;
-                                _driversCount = drivers.Count;
-                            }
-                            _loadedCount++;
+                            var entryStream = new MemoryStream();
+                            using var rawEntryStream = entry.Open();
+                            rawEntryStream.CopyTo(entryStream);
+                            entryStream.Position = 0;
+                            entryLoaders.AddLast(Task.Run(_getEntryStreamHandler(entryStream)));
                         }
-                        _drivers = drivers.ToArray();
                     }
+                    _loadDiskMs = (long)(DateTime.Now - firstTime).TotalMilliseconds;
+                    Task.WhenAll(entryLoaders);
+                    int index = 0;
+                    foreach (var task in entryLoaders)
+                    {
+                        if (task.Result == null)
+                            continue;
+                        index++;
+                        task.Result.Id = index;
+                        drivers.Add(task.Result);
+                    }
+                    // 转换为数组
+                    _drivers = drivers.ToArray();
+                    _loadTotalMs = (long)(DateTime.Now - firstTime).TotalMilliseconds;
                 }
                 catch (Exception error)
                 {
