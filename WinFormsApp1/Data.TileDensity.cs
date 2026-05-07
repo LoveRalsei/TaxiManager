1ba7a8cba6de568b1b284976d486a316ed1e2b30
SaltfishSheep <saltfishsheep@hotmail.com>
2026-05-07 22:36:30 +0800

    F4做到一半

--- WinFormsApp1/Data.TileDensity.cs
+++ WinFormsApp1/Data.TileDensity.cs
@@ -9,5 +9,23 @@ namespace TaxiManager
     public class TileDensity
     {
         private readonly static Dictionary<(Tile tile, DateTime time), uint> _densityMap = [];
+        private static Task? _task;
+        public static bool Loaded => _task?.IsCompleted ?? false;
+        public static bool IsError => _task?.IsFaulted ?? false;
+        public static Exception? Error => _task?.Exception;
+        public static void Init()
+        {
+            _task = Task.Run(() =>
+            {
+                var drivers = DataLoader.Drivers;
+                foreach (var driver in drivers)
+                {
+                    foreach (var node in driver.Nodes)
+                    {
+                        //if (_densityMap.TryGetValue((node.Position.GetTile())))
+                    }
+                }
+            });
+        }
     }
 }
