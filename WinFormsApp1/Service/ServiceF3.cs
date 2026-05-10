e14d8384858279c66f3f35405180439095677574
SaltfishSheep <saltfishsheep@hotmail.com>
2026-05-10 09:49:09 +0800

    ä¼F3性能优化

--- WinFormsApp1/Service/ServiceF3.cs
+++ WinFormsApp1/Service/ServiceF3.cs
@@ -15,15 +15,14 @@ namespace TaxiManager.Service
             if (from > to)
                 throw new ArgumentException("The arg from should not be greater than the arg to.");
             uint count = 0;
-            HashSet<Tile> rangeTiles = [.. range.GetTiles(Driver.ExistTileSize)];
             foreach (var driver in DataLoader.Drivers)
             {
-                if (driver.IsEmpty || !driver.IsExist(from, to) || !driver.IsExist(rangeTiles))
+                if (driver.IsEmpty)
+                    continue;
+                if (!driver.IsExist(from, to))
                     continue;
                 var fromIndex = driver.GetPositionIndex(from);
                 var toIndex = driver.GetPositionIndex(to);
-                if (fromIndex == null && toIndex == null)
-                    continue;
                 fromIndex ??= (0, 0, driver.Nodes[0].Position);
                 toIndex ??= ((uint)driver.Nodes.Count - 1, (uint)driver.Nodes.Count - 1, driver.Nodes.Last().Position);
                 // 如果插值左节点相同，说明其在同一个插值范围，只需要判定左右两侧节点是否在范围中
@@ -35,15 +34,13 @@ namespace TaxiManager.Service
                     continue;
                 }
                 // 左侧插值节点取左，右侧插值节点取右，这样可以获取到的结果判定相对宽松
-                uint left = fromIndex.Value.indexLeft;
-                uint right = toIndex.Value.indexRight;
+                var left = fromIndex.Value.indexLeft;
+                var right = toIndex.Value.indexRight;
                 for (int i = (int)left; i <= right; i++)
                 {
-                    if (range.IsIn(driver.Nodes[i].Position))
-                    {
-                        count++;
-                        break;
-                    }
+                    if (!range.IsIn(driver.Nodes[i].Position)) continue;
+                    count++;
+                    break;
                 }
             }
             return count;
