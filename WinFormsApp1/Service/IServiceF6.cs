cc9849aeda0e26acd8868b3a545595796bd3a5e1
SaltfishSheep <saltfishsheep@hotmail.com>
2026-05-11 11:26:40 +0800

    F6基本实现

--- WinFormsApp1/Service/IServiceF6.cs
+++ WinFormsApp1/Service/IServiceF6.cs
@@ -1,6 +1,9 @@
+using TaxiManager.Structure;
+
 namespace TaxiManager.Service;
 
 public interface IServiceF6
 {
-    
+    public static IServiceF6 Instance => ServiceF6.Instance;
+    public int GetFlowTo(PositionRange toRange, DateTime time);
 }
